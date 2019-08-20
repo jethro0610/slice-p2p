8a01922d5d25cad3ffb079e69bb765887fc5c499
JetDesktop <jethro0610@outlook.com>
2019-08-19 18:09:32 -0700

    Add host list

--- index.js
+++ index.js
@@ -6,6 +6,16 @@ var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 var ExpressPeerServer = require('peer').ExpressPeerServer;
 
+var hostingClients = [];
+var connectedClients = [];
+
+class networkClient {
+	constructor(newClientSocket, newClientID) {
+		this.socket = newClientSocket;
+		this.id = newClientID;
+	}
+}
+
 // Static files
 app.use(express.static('public'));
 
@@ -28,7 +38,41 @@ io.on('connection', function(socket){
 	
 	socket.on('requestClientID', function(){
 		console.log('client id requested');
-		var generatedID = uniqid.time();
-		socket.emit('sendClientID', generatedID);
+		// Only assign an ID if the client doesn't have one
+		if(getClientFromSocket(socket, connectedClients) == null){
+			var generatedID = uniqid.time();
+			connectedClients.push(new networkClient(socket, generatedID));
+			socket.emit('sendClientID', generatedID);
+		}
+	});
+
+	socket.on('requestHost', function(){
+		var requestingClient = getClientFromSocket(socket, connectedClients);
+		if(requestingClient != null && getClientFromSocket(socket, hostingClients) == null)
+			hostingClients.push(requestingClient);
+	});
+
+	socket.on('requestHostList', function(){
+		var hostListToSend = [];
+		for (var i = 0; i < hostingClients.length; i++) {
+			hostListToSend.push(hostingClients[i].id);
+		}
+		socket.emit('sendHostList', hostListToSend);
 	});
 });
+
+function getClientFromID(idToCheck, arrayToCheck){
+	for (var i = 0; i < arrayToCheck.length; i++) {
+		if(arrayToCheck[i].id == idToCheck)
+			return arrayToCheck[i];
+	}
+	return null;
+}
+
+function getClientFromSocket(socketToCheck, arrayToCheck){
+	for (var i = 0; i < arrayToCheck.length; i++) {
+		if(arrayToCheck[i].socket == socketToCheck)
+			return arrayToCheck[i];
+	}
+	return null;
+}
--- public/scripts/matchmaking.js
+++ public/scripts/matchmaking.js
@@ -1,10 +1,25 @@
 var matchmakingServer = io();
+requestClientID();
+var hostList = []
 
 function requestClientID(){
 	matchmakingServer.emit('requestClientID');
 }
 
+function requestHost(){
+	matchmakingServer.emit('requestHost');
+}
+
+function requestHostList(){
+	matchmakingServer.emit('requestHostList');
+}
+
 matchmakingServer.on('sendClientID', function(newClientID){
 	console.log('recieved client id: ' + newClientID.toString());
 	setLocalClient(new Peer(newClientID, {host: 'localhost', port: 3000, path:'/api'}));
+});
+
+matchmakingServer.on('sendHostList', function(newHostList){
+	hostList = newHostList;
+	console.log(hostList);
 });
